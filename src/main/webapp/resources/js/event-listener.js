472e17e412119ebec800b4f1208ca4e7cdf89e92
Gromak Maxim <maximgromak@gmail.com>
2021-08-30 16:13:16 +1000

    upd: redundant code has been removed

--- src/main/webapp/resources/js/event-listener.js
+++ src/main/webapp/resources/js/event-listener.js
@@ -9,22 +9,8 @@ export function initCardSelection() {
                 deselect(cards);
                 card.classList.add("selected-card");
             }
-            hourlyCardSelection();
         })
     }
-}
-
-function hourlyCardSelection() {
-    let cards = document.getElementsByClassName("field");
-    let selected = false;
-    for (let card of cards) {
-        if (card.classList.contains("selected-card")) {
-            selected = true;
-            document.getElementsByClassName("weather-details")[0].classList.remove("hidden");
-            break;
-        }
-    }
-    if (!selected) document.getElementsByClassName("weather-details")[0].classList.add("hidden");
 }
 
 function deselect(cards) {
